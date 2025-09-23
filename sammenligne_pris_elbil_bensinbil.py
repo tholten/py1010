f8095f0fe099f2da627f52c7f1066b6c1bba7305
tholten <54404860+tholten@users.noreply.github.com>
2025-09-23 22:53:50 +0200

    Update sammenligne_pris_elbil_bensinbil.py
    La til et par kommentarer og fjernet noen unødvendige linjer.

--- sammenligne_pris_elbil_bensinbil.py
+++ sammenligne_pris_elbil_bensinbil.py
@@ -29,8 +29,8 @@ bom_el = 0.1 * kilometer_arlig # [kr]
 bom_bensin = 0.3 * kilometer_arlig # [kr]
 
 # Pris pr. år
-arlig_kostnad_el = forsikring_el + trafikkforsikringsavgift + drivstoffbruk_el + bom_el
-arlig_kostnad_bensin = forsikring_bensin + trafikkforsikringsavgift + drivstoffbruk_bensin + bom_bensin
+arlig_kostnad_el = forsikring_el + trafikkforsikringsavgift + drivstoffbruk_el + bom_el # [kr]
+arlig_kostnad_bensin = forsikring_bensin + trafikkforsikringsavgift + drivstoffbruk_bensin + bom_bensin  # [kr]
 
 # Kostnadsdifferanse pr. år.
 # Funksjonen tar høyde for at elbiblkostnader kan overstige bensinbilkostnader
@@ -46,20 +46,6 @@ print("Elbil kostnad pr. år: " + str(round(arlig_kostnad_el)) + " kr")
 print("Bensinbil kostnad pr. år: " + str(round(arlig_kostnad_bensin)) + " kr")
 print("Kostnadsdifferanse: " + str(round(arlig_kostnadsdifferanse())) + " kr")
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
